cdc0994c4d1f62e8b98095fdbfd9b78de980af4c
Allen Wong <allenjameswong@gmail.com>
2019-04-10 15:21:45 -0700

    Added a scrollview to hold different metronome visual designs. Created a folder to demonstrate how to make a view + xib where the design will go. Added // MARK''s to make navigating project easier.

---
+++ HapticTouch/Metronome Graphic Views/MetronomeDesign.swift
@@ -0,0 +1,15 @@
+//
+//  MetronomeDesign.swift
+//  HapticTouch
+//
+//  Created by Allen Wong on 4/10/19.
+//  Copyright © 2019 Coding Buddies. All rights reserved.
+//
+
+import UIKit
+
+class MetronomeDesign: UIView {
+    @IBOutlet weak var viewLabel: UILabel!
+    @IBOutlet weak var bpmLabel: UILabel!
+    
+}
--- HapticTouch/MetronomeVC.swift
+++ HapticTouch/MetronomeVC.swift
@@ -9,30 +9,18 @@
 import UIKit
 import AudioToolbox
 
-extension UIDevice {
-    var modelName: String {
-        var systemInfo = utsname()
-        uname(&systemInfo)
-        let machineMirror = Mirror(reflecting: systemInfo.machine)
-        let identifier = machineMirror.children.reduce("") { identifier, element in
-            guard let value = element.value as? Int8, value != 0 else { return identifier }
-            return identifier + String(UnicodeScalar(UInt8(value)))
-        }
-        return identifier
-    }
-}
-
-class MetronomeVC: UIViewController {
-
+class MetronomeVC: UIViewController, UIScrollViewDelegate {
+    // MARK: - Outlets
+    @IBOutlet weak var metronomeDesignsScrollView: UIScrollView!
+    @IBOutlet weak var metronomeDesignsPageControl: UIPageControl!
     @IBOutlet weak var hapticFeedbackButton: UIButton!
     @IBOutlet weak var bpmSliderControl: UISlider!
     @IBOutlet weak var bpmNumberLabel: UILabel!
-    
+
+    // MARK: - Properties
     let startString = NSLocalizedString("startMetronome", comment: "Start metronome")
     let stopString = NSLocalizedString("stopMetronome", comment: "Stop metronome")
-    
-    // check if device is an iPhone 6 or iPhone 6s (no support for impact generator)
-    // iPhone 6 models begin with string iPhone8
+    var metronomeDesigns = [MetronomeDesign]()
     let metronome = Metronome(bpm: 60, supportsImpactGenerator: !UIDevice.current.modelName.starts(with: "iPhone8"))
     var metronomeRunningStatus = false {
         didSet {
@@ -44,23 +32,46 @@ class MetronomeVC: UIViewController {
         }
     }
 
+    // MARK: - View Controller Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        metronomeDesignsScrollView.delegate = self
+        metronomeGraphicsSetup()
+        metronomeGraphicPageControlSetup()
         metronomeRunningStatus = false
-        
         let value = Int(bpmSliderControl.value)
         bpmNumberLabel.text = "\(value)"
         metronome.setBPM(to: value)
+        updateFieldsInMetronomeGraphicView(bpm: "\(value)")
     }
 
+    // MARK: - Setup Functions
+    func metronomeGraphicsSetup() {
+        metronomeDesigns = createMetronomeDesigns()
+        setupScrollView(metronomeStyles: metronomeDesigns)
+        view.bringSubviewToFront(metronomeDesignsPageControl)
+    }
+
+    func metronomeGraphicPageControlSetup() {
+        metronomeDesignsPageControl.numberOfPages = metronomeDesigns.count
+        metronomeDesignsPageControl.currentPage = 0
+    }
+
+    func setupScrollView(metronomeStyles: [MetronomeDesign]) {
+        metronomeDesignsScrollView.contentSize = CGSize(width: view.frame.width * CGFloat(createMetronomeDesigns().count), height: metronomeDesignsScrollView.frame.height)
+        metronomeDesignsScrollView.isPagingEnabled = true
+
+        for design in 0 ..< metronomeStyles.count {
+            metronomeStyles[design].frame = CGRect(x: view.frame.width * CGFloat(design), y: 0, width: view.frame.width, height: metronomeDesignsScrollView.frame.height)
+            metronomeDesignsScrollView.addSubview(metronomeStyles[design])
+        }
+    }
+
+    // MARK: - Actions
     @IBAction func hapticFeedbackButton(_ sender: Any) {
         metronome.toggle()
         metronomeRunningStatus = metronome.isRunning()
-    }
-
-    func timerActionFallback() {
-        AudioServicesPlaySystemSound(1520)
     }
 
     @IBAction func minusButtonPressed(_ sender: Any) {
@@ -84,14 +95,63 @@ class MetronomeVC: UIViewController {
             metronome.setBPM(to: value)
             bpmSliderControl.setValue(Float(value), animated: true)
         }
-        
     }
     
     @IBAction func bpmSliderValueChanged(_ sender: UISlider) {
         let currentMetronomeSpeed = Int(sender.value)
         bpmNumberLabel.text = "\(currentMetronomeSpeed)"
         metronome.setBPM(to: currentMetronomeSpeed)
+        updateFieldsInMetronomeGraphicView(bpm: "\(currentMetronomeSpeed)")
+
     }
 
+    // MARK: - Functions (Better name for this mark?)
+    func timerActionFallback() {
+        AudioServicesPlaySystemSound(1520)
+    }
 
+    // Example of how to access properties of the currently selected Metronome Design in the scrollview
+    func updateFieldsInMetronomeGraphicView(bpm: String) {
+        let currentPageIndex = metronomeDesignsPageControl.currentPage
+        let currentlySelectedDesign = metronomeDesigns[currentPageIndex]
+        currentlySelectedDesign.bpmLabel.text = bpm
+    }
+
+    // Load your design by changing to the relative nib name you created
+    // Add it to the array returned below
+    func createMetronomeDesigns() -> [MetronomeDesign] {
+        let viewOne = Bundle.main.loadNibNamed("MetronomeDesign", owner: self, options: nil)?.first as! MetronomeDesign
+        viewOne.viewLabel.text = "View 1"
+        viewOne.backgroundColor = UIColor.red
+
+        let viewTwo = Bundle.main.loadNibNamed("MetronomeDesign", owner: self, options: nil)?.first as! MetronomeDesign
+        viewTwo.backgroundColor = UIColor.blue
+        viewTwo.viewLabel.text = "View 2"
+
+        let viewThree = Bundle.main.loadNibNamed("MetronomeDesign", owner: self, options: nil)?.first as! MetronomeDesign
+        viewThree.viewLabel.text = "View 3"
+        viewThree.backgroundColor = UIColor.gray
+
+        return [viewOne, viewTwo, viewThree]
+    }
+
+    // MARK: - ScrollView Delegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = Int(round(metronomeDesignsScrollView.contentOffset.x / view.frame.width))
+        metronomeDesignsPageControl.currentPage = pageIndex
+    }
+}
+
+// MARK: - Extensions
+extension UIDevice {
+    var modelName: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
+    }
 }
